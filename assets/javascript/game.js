94c5b1afabf00beedd92e56d1be98baede930955
morralex <morr.alex@outlook.com>
2019-06-16 20:34:03 -0700

    pseudo

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,7 +4,37 @@ $(document).ready(function(){
 
 var Dealer = Math.floor(Math.random() * 101 + 19)
 console.log(dealer)
-// funtion that recals the game
+// Selects a random number to be shown at the start of the game
+    // Number should be should be between 19 - 120
+    //
+    $('#randomNumber').text(dealer);
+    // Appending random number to the randomNumber id in the html doc
+
+
+    //// Setting up random numbers for each jewel
+    // Random number has to be between 1 - 12
+    var num1 = Math.floor(Math.random() * 11 + 1)
+    var num2 = Math.floor(Math.random() * 11 + 1)
+    var num3 = Math.floor(Math.random() * 11 + 1)
+    var num4 = Math.floor(Math.random() * 11 + 1)
+    
+    
+    var userTotal = 0;
+    var wins = 0;
+    var losses = 0;
+    //  Decaring variables for tallies
+    
+    //reset the game
+    
+    //add wins to the userTotal
+    
+    //add losses to the userTotal
+    
+    //sets up click for jewels
+    
+    //sets win/lose conditions
+        
+    //sets win/lose conditions
 
 
 
