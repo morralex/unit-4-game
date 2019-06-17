fd4a86639ca60b5ee9b73c0f4054af467966ab3c
morralex <morr.alex@outlook.com>
2019-06-16 20:23:43 -0700

    style

---
+++ assets/javascript/game.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+
+//select a random number between 19 and 20
+
+var Dealer = Math.floor(Math.random() * 101 + 19)
+console.log(dealer)
+// funtion that recals the game
+
+
+
+
+
+
+})
