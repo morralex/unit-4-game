a56c73d78fb527759cc1a4ec19d4978ebad140a7
morralex <morr.alex@outlook.com>
2019-06-17 15:15:30 -0700

    js file linked

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,13 +1,13 @@
-$(document).ready(function(){
+$(document).ready(function () {
 
-//select a random number between 19 and 20
+    //select a random number between 19 and 20
 
-var Dealer = Math.floor(Math.random() * 101 + 19)
-console.log(dealer)
-// Selects a random number to be shown at the start of the game
+    var dealer = Math.floor(Math.random() * 101 + 19);
+    console.log(dealer);
+    // Selects a random number to be shown at the start of the game
     // Number should be should be between 19 - 120
     //
-    $('#randomNumber').text(dealer);
+    $('#dealerNum').html(dealer);
     // Appending random number to the randomNumber id in the html doc
 
 
@@ -17,23 +17,23 @@ console.log(dealer)
     var num2 = Math.floor(Math.random() * 11 + 1)
     var num3 = Math.floor(Math.random() * 11 + 1)
     var num4 = Math.floor(Math.random() * 11 + 1)
-    
-    
+
+
     var userTotal = 0;
     var wins = 0;
     var losses = 0;
     //  Decaring variables for tallies
-    
+
     //reset the game
-    
+
     //add wins to the userTotal
-    
+
     //add losses to the userTotal
-    
+
     //sets up click for jewels
-    
+
     //sets win/lose conditions
-        
+
     //sets win/lose conditions
 
 
