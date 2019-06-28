2cdc311375593d94c35bdc7157f80bf9976a520e
morralex <morr.alex@outlook.com>
2019-06-28 07:02:39 -0700

    bug fix

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -32,7 +32,7 @@ $(document).ready(function () {
     function reset() {
         dealer = Math.floor(Math.random() * 101 + 19);
 
-        $('dealerNum').html(dealer);
+        $('#dealerNum').html(dealer);
         num1 = Math.floor(Math.random() * 11 + 1)
         num2 = Math.floor(Math.random() * 11 + 1)
         num3 = Math.floor(Math.random() * 11 + 1)
